68182cb03d730798bd75adf1cbbe2322c7b285ba
Nicolas Fosdick <nick.fosdick@sigfig.com>
2018-01-25 15:39:17 -0700

    More updates

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,19 +2,36 @@ class fusion(
   $install_dir = '/opt/fusion',
   $version     = '3.1.2',
   $url         = "https://s3-us-west-2.amazonaws.com/red-software/fusion-${version}.tar.gz",
+  $lvsize      = '30G',
+  $lvconfig    = {},
 ) {
-#  include archive
+
+  $fusion_config_defaults = {
+    'fusion_vg' =>
+    {
+      'physical_volumes' => ['/dev/xvdf'],
+      'logical_volumes' => {
+        'fusionlv' => {
+          'size'      => $lvsize,
+          'mountpath' => $install_dir
+        }
+      }
+     }
+  }
 
   file{ '/opt/fusion':
     ensure => directory
   }
 
-  #archive { "${install_dir}/fusion-${version}.tar.gz":
+  class { 'lvm':
+    volume_groups => $fusion_config_defaults,
+  }
+
   archive { "/opt/fusion-${version}.tar.gz":
     source       => $url,
     extract      => true,
     extract_path => '/opt',
-    creates      => '/opt/fusion/3.1.2',
+    creates      => "${install_dir}/${version}",
     cleanup      => true,
   }
 
