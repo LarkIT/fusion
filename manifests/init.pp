938a70ee80386b0aa444d067cf5affcef644c7e4
Nicolas Fosdick <nick.fosdick@sigfig.com>
2018-01-25 13:54:07 -0700

    More updates

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,14 @@ class fusion(
   $version     = '3.1.2',
   $url         = "https://s3-us-west-2.amazonaws.com/red-software/fusion-${version}.tar.gz",
 ) {
-  include archive
+#  include archive
 
   file{ '/opt/fusion':
     ensure => directory
   }
 
-  archive { "${install_dir}/fusion-${version}.tar.gz":
+  #archive { "${install_dir}/fusion-${version}.tar.gz":
+  archive { "/opt/fusion-${version}.tar.gz":
     source        => $url,
     extract       => true,
     extract_path  => $install_dir,
