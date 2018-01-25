5ea7a76e9265f39388a948b608a7c9186a679bc6
Nicolas Fosdick <nick.fosdick@sigfig.com>
2018-01-25 13:35:42 -0700

    More updates

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,17 @@
-class fusion {
-  notify{"Nick":}
+class fusion(
+  $install_dir = '/opt/fusion',
+  $url         = 'https://s3-us-west-2.amazonaws.com/red-software/fusion-3.1.2.tar.gz'
+) {
+
   file{ '/opt/fusion':
     ensure => directory
   }
 
+  archive { $install_dir,
+    source        => $url,
+    extract       => true,
+    extract_path  => $install_dir,
+    creates       => "${homedir}/help" #directory inside tgz
+  }
 
 }
