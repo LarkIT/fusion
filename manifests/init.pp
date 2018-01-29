c6d490f8706a0d95b3aa946cd89eec7df9378127
Nicolas Fosdick <nick.fosdick@sigfig.com>
2018-01-29 09:57:53 -0700

    Updating hash

--- manifests/init.pp
+++ manifests/init.pp
@@ -65,8 +65,8 @@ class fusion(
 
   $hiera_ssh_keys = lookup('host_railsapp::global_ssh_keys', Hash, "first")
 
-  $hiera_ssh_keys.each |$key| {
-    notify{"Key = $key[name]":} 
+  $hiera_ssh_keys.each |$key,$value| {
+    notify{"Key = $key":} 
   } 
 
 }
