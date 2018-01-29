66d102213d34476e16cf583e633d875cf9aa584e
Nicolas Fosdick <nick.fosdick@sigfig.com>
2018-01-29 09:16:25 -0700

    Updating hash

--- manifests/init.pp
+++ manifests/init.pp
@@ -63,7 +63,7 @@ class fusion(
     refreshonly => true,
   }
 
-  $hiera_ssh_keys       = lookup("host_railsapp::global_ssh_keys", Struct[ Hash[String,String]], "first")
+  $hiera_ssh_keys       = lookup("host_railsapp::global_ssh_keys", Hash[String,String], "first")
 
   notify{"Nick $hiera_ssh_keys":}
     # SSH Keys
