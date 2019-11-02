06feee46ebc8732e696de7f505977cd5bfc4da8b
Ethan Claassen <ethan.claassen@lark-it.com>
2019-11-01 18:03:26 -0600

    Adding modified config files

--- manifests/init.pp
+++ manifests/init.pp
@@ -79,14 +79,19 @@ class fusion(
   # Copy configuration files
   
    
-  file { "/opt/fusion/${version}/conf/fusion.properties":
-  ensure  => file,
+  file { "/opt/fusion/${version}/conf":
+  ensure  => directory,
+  require => Archive[ "/opt/fusion-${version}.tar.gz" ],
   owner   => 'fusion',
   group   => 'fusion',
   mode    => '0644',
   notify  => Service['fusion'],
   source  => "puppet:///modules/${module_name}/${version}/conf/fusion.properties",
+  recurse => 'remote'
   }
+
+  
+  
 
   # Install init script
 
