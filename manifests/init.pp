81114a877f1bb16a962da033690d3a2887758993
Ethan Claassen <ethan.claassen@lark-it.com>
2019-11-22 12:50:41 -0700

    Updated confguration files

--- manifests/init.pp
+++ manifests/init.pp
@@ -92,15 +92,15 @@ class fusion(
     source  => "puppet:///modules/${module_name}/${version}/conf/agent-log4j2.xml",
   }
 
-  file { "/opt/fusion/${version}/conf/api-log4j2.xml":
-    ensure  => file,
-    require => Archive[ "/opt/fusion-${version}.tar.gz" ],
-    owner   => 'fusion',
-    group   => 'fusion',
-    mode    => '0644',
-    notify  => Service['fusion'],
-    source  => "puppet:///modules/${module_name}/${version}/conf/api-log4j2.xml",
-  }
+  #file { "/opt/fusion/${version}/conf/api-log4j2.xml":
+  #  ensure  => file,
+  #  require => Archive[ "/opt/fusion-${version}.tar.gz" ],
+  #  owner   => 'fusion',
+  #  group   => 'fusion',
+  #  mode    => '0644',
+  #  notify  => Service['fusion'],
+  #  source  => "puppet:///modules/${module_name}/${version}/conf/api-log4j2.xml",
+  #}
 
 
 
